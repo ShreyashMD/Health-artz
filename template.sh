a851b46be466ea5cf1bd9b4173d2ae4c9b365f5c
ShreyashMD <deokateshreyash1967@gmail.com>
2025-07-24 15:06:06 +0200

    folders

---
+++ template.sh
@@ -0,0 +1,17 @@
+#creating dir
+
+mkdir -p src
+mkdir -p research
+
+# create a files
+
+touch src/__inti__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirnments.txt
+
+echo "directory and files sucessfully created"
