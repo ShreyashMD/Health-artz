7f363c85f59c6a0e6542c58a4c45a09793b6fc98
ShreyashMD <deokateshreyash1967@gmail.com>
2025-07-24 15:30:56 +0200

    requirnments added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+name= "Health-artz",
+version="0.1.0",
+author= "Shreyash Manohar Deokate",
+author_email="deokateshreyash1967@gmail.com",
+packages= find_packages(),
+install_requires= []
+)
